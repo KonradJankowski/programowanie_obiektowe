92e897142d35fb97994600c5ac215ceb3bd82692
Konrad Jankowski <elvonet@gmail.com>
2022-12-22 11:42:59 +0100

    Refactor konstruktory.py

--- konstruktor.py
+++ konstruktor.py
@@ -1,26 +1,55 @@
+import random
+
+
+# Obiekt może zawierać w sobie inne obiekty
+class School:
+
+    def __init__(self,name ,students):
+        self.name = name
+        if len(students) > 10:
+            students = students[:10]
+        self.students = students
+
+
 class Student:
-    # Konstruktor zostanie wywołąny podczas tworzenia obiektu
-    def __init__(self):
-        self.first_name = "Konrad"
-        self.last_name = "Jankowski"
-        self.age = 18
 
-def example():
-    student = Student()
-    # Do stanu obiektu mamy dostęp - możmy go odczytać
-    print(student.first_name)
-    print(student.last_name)
-    print(student.age)
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.promoted = False
 
-def second_example():
-    student = Student()
-    student.first_name = "Aleksander"
-    student.age = 12
-    print(student.first_name)
-    print(student.last_name)
-    print(student.age)
+
+def print_student(student):
+    print(f"Student: {student.first_name} {student.last_name}, promoted {student.promoted}")
+
+
+def promote_student(student):
+    student.promoted = True
+
+
+# Funkcja może zwracać obiekty
+def create_school_with_students(school_name, number_of_students):
+    # number_of_students = random.randint(1, 20) #losowanie liczby studentów
+    students = []
+    for student_number in range(number_of_students):
+        first_name = f"Student-{student_number}-{school_name}"
+        last_name = "Smith"
+        students.append(Student(first_name, last_name))
+
+    school = School(school_name, students)
+    return school
+
+def run_example():
+    school = create_school_with_students("Hogwart", 4)
+    school_kucykowo = create_school_with_students("Kucykowo", 30)
+    print(school)
+    print(school_kucykowo)
+    for student in school.students:
+        print_student(student)
+    for student in school_kucykowo.students:
+        print_student(student)
+
 
 
 if __name__ == '__main__':
-    example()
-    second_example()
+    run_example()
